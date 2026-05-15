c1d3ff09f3b022d3a5a6c13402052a43ec045eb4
petereog <ogungbepeter1@gmail.com>
2026-05-16 03:25:55 +1200

    added an update for today

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const helloRoute = require('./routes/hello');
 
 const PORT = 3000;
 
 const app = express();
+const DB = "mongodb+srv://petereniolaogungbe:peterEniola@cluster0.rkhvpkr.mongodb.net/?appName=Cluster0"
 
 app.use(helloRoute);
+mongoose.connect(DB).then(() => {
+    console.log('connected to mongo');
+}).catch(err => {
+    console.log('Database connection error:', err.message);
+});
+
+
 
 app.listen(PORT, "0.0.0.0", function () {
     console.log(`server is running on port ${PORT}`);
